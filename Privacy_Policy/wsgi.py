ab23786ccaf2430ea6fd48f5b8c89be65d5d0a75
Erad <eriadelowo@gmail.com>
2025-06-23 18:54:04 +0100

    first commit

---
+++ Privacy_Policy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Privacy_Policy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Privacy_Policy.settings')
+
+application = get_wsgi_application()
